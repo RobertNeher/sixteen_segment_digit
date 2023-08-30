a09e4c6dc829e84f338448e213fba1ba91fc9f3a
RobertNeher <Robert.Neher@gmail.com>
2023-08-30 23:47:38 +0200

    Fixing segment map

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               CustomPaint(
                   painter: DigitPainter(
-                      symbol: ' ',
+                      symbol: 'A',
                       foreground: Colors.red,
                       background: Colors.white)),
             ])));
--- lib/segment_painter.dart
+++ lib/segment_painter.dart
@@ -6,7 +6,7 @@ class DigitPainter extends CustomPainter {
   Color background;
 
   DigitPainter({
-    this.symbol = ' ',
+    this.symbol = '',
     this.foreground = Colors.red,
     this.background = Colors.yellowAccent,
   }) {
@@ -17,9 +17,105 @@ class DigitPainter extends CustomPainter {
   static const double opacity = 0.5;
   static const String segmentIndex = 'abhkmncupgtsrdfe.\'';
   static const Map<String, String> segmentMap = {
+    'all': '11111111111111110',
     //    012345678901234567
+    //    abh kmncupgtsrdfe.'
+    ' ': '000000000000000000',
+    '!': '000000100000010010',
+    '"': '000010100000000000',
+    '#': '000010111001011100',
+    '\$': '11101001100101110',
+    '%': '101011011011010100',
+    '&': '100110010100101100',
+    '\'': '000010000000000000',
+    '(': '000001000000100000',
+    ')': '000100000010000000',
+    '*': '000111011011100000',
+    '+': '000010011001000000',
+    ',': '000000000010000000',
+    '-': '000000011000000000',
+    '.': '000000000000000010',
+    '/': '000001000010000000',
+    '0': '111001100110011100',
+    '1': '000001100000010000',
+    '2': '110000111100001100',
+    '3': '110000111000011100',
+    '4': '001000111000010000',
+    '5': '111000010000101100',
+    '6': '111000011100011100',
+    '7': '110000100000010000',
+    '8': '111000111100011100',
+    '9': '111000111000011100',
+    ':': '000010000001000000',
+    ';': '000010000010000000',
+    '<': '000001010000100000',
+    '=': '000000011000001100',
+    '>': '000100001010000000',
+    '?': '110000101001000010',
+    '@': '111010101100001100',
+    'A': '111000111100010000',
     //    abhkmncupgtsrdfe.'
-    ' ': '111111111111111111',
+    'B': '001010100011111100',
+    'C': '000000001111001100',
+    'D': '001000100011111100',
+    'E': '100000001111001100',
+    'F': '100000001100001100',
+    'G': '000010001111101100',
+    'H': '100010001100110000',
+    'I': '001000100011001100',
+    'J': '000000000111110000',
+    'K': '100101001100000000',
+    'L': '000000001111000000',
+    'M': '000001011100110000',
+    'N': '000100011100110000',
+    'O': '000000001111111100',
+    'P': '100010001100011100',
+    'Q': '000100001111111100',
+    'R': '100110001100011100',
+    'S': '100010001011101100',
+    'T': '001000100000001100',
+    'U': '000000001111110000',
+    'V': '010001001100000000',
+    'W': '010100001100110000',
+    'X': '010101010000000000',
+    'Y': '100010001011110000',
+    'Z': '010001000011001100',
+    '[': '001000100001001000',
+    '\\': '000100010000000000',
+    ']': '001000100010000100',
+    '^': '010100000000000000',
+    '_': '000000000011000000',
+    '`': '000000010000000000',
+    'a': '101000000111000000',
+    'b': '101000001110000000',
+    'c': '100000000110000000',
+    'd': '001010000001110000',
+    'e': '110000000110000000',
+    'f': '101010100000001000',
+    'g': '101000101010000100',
+    'h': '101000001100000000',
+    'i': '001000000000000000',
+    'j': '001000100110000000',
+    'k': '001101100000000000',
+    'l': '000000001100000000',
+    'm': '101010000100100000',
+    'n': '101000000100000000',
+    'o': '101000000110000000',
+    'p': '100000101100000100',
+    'q': '101000101000000100',
+    'r': '100000000100000000',
+    's': '101000001010000100',
+    't': '100000001110000000',
+    'u': '001000000110000000',
+    'v': '010000000100000000',
+    'w': '010100000100100000',
+    'x': '010101010000000000',
+    'y': '000010100001110000',
+    'z': '110000000010000000',
+    '{': '101000100001001000',
+    '|': '001000100000000000',
+    '}': '001010100010000100',
+    '~': '110011000000000000',
   };
 
   bool _segmentOnOff(int index) {
@@ -48,7 +144,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(0 + offset_x, 10 + offset_y);
       path.relativeLineTo(20, -20);
@@ -64,7 +159,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(190 + offset_x, -10 + offset_y);
       path.relativeLineTo(120, 0);
@@ -80,7 +174,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(-25 + offset_x, 35 + offset_y);
       path.relativeLineTo(20, -17);
@@ -97,7 +190,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(25 + offset_x, 100 + offset_y);
       path.relativeLineTo(10, -42);
@@ -115,7 +207,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(145 + offset_x, 55 + offset_y);
       path.relativeLineTo(30, 0);
@@ -133,7 +224,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(265 + offset_x, 55 + offset_y);
       path.relativeLineTo(10, 0);
@@ -150,7 +240,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(290 + offset_x, 50 + offset_y);
       path.relativeLineTo(40, -35);
@@ -167,7 +256,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(-50 + offset_x, 275 + offset_y);
       path.relativeLineTo(40, -30);
@@ -183,7 +271,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(135 + offset_x, 245 + offset_y);
       path.relativeLineTo(115, 0);
@@ -199,7 +286,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(-70 + offset_x, 285 + offset_y);
       path.relativeLineTo(20, 0);
@@ -216,7 +302,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(60 + offset_x, 310 + offset_y);
       path.relativeLineTo(30, 0);
@@ -233,7 +318,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(115 + offset_x, 310 + offset_y);
       path.relativeLineTo(20, 0);
@@ -250,7 +334,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(150 + offset_x, 310 + offset_y);
       path.relativeLineTo(27, 0);
@@ -267,7 +350,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(240 + offset_x, 320 + offset_y);
       path.relativeLineTo(50, -30);
@@ -285,7 +367,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(-90 + offset_x, 537 + offset_y);
       path.relativeLineTo(40, -30);
@@ -301,7 +382,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(105 + offset_x, 505 + offset_y);
       path.relativeLineTo(100, 0);
@@ -317,7 +397,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.addOval(Rect.fromCircle(
           center: Offset(320 + offset_x, 525 + offset_y), radius: 35));
@@ -329,7 +408,6 @@ class DigitPainter extends CustomPainter {
       paint.color = _segmentOnOff(segmentIndex.indexOf(segment))
           ? foreground
           : background;
-      print(segment);
       path = Path();
       path.moveTo(-75 + offset_x, -10 + offset_y);
       path.relativeLineTo(40, 0);
